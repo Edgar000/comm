8df510e242f40e78a241d016383fc570d1efa5b9
Edgar000 <edgar026026@gmail.com>
2016-06-13 23:11:21 +0300

    test form added

--- app/wall/newComment/newComment.module.js
+++ app/wall/newComment/newComment.module.js
@@ -4,6 +4,14 @@ import NewComment from './newComment.component';
 const MODULE_NAME = 'app.wall.newComment';
 const module = angular.module(MODULE_NAME, []);
 
+module.controller('newCommentController', ['$scope', function($scope) {
+    $scope.saved = {};
+    $scope.save = function(comment) {
+        $scope.saved = angular.copy($scope.comment);
+        $scope.saved = angular.copy()
+    };
+}]);
+
 module.component('newComment', NewComment);
 
 export {module, MODULE_NAME as default};
